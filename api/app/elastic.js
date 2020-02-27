691680bb4f7bce1e524ce452fb327d675aa740ff
Kevin Chen <kvn.0218+github@gmail.com>
2020-02-27 09:34:36 +0000

    add header to req and res

--- api/app/elastic.js
+++ api/app/elastic.js
@@ -1,107 +1,117 @@
-const items = require('../db/items.json')
-const elasticsearch = require('elasticsearch')
+const items = require("../db/items.json");
+const elasticsearch = require("elasticsearch");
 
 const createClient = async () => {
-    return elasticsearch.Client({
-        hosts: [(process.env.ES_HOST || `http://localhost:9200`)],
-        maxRetries: 30,
-        requestTimeout: 30000
-    })
-}
+  return elasticsearch.Client({
+    hosts: [process.env.ES_HOST || `http://localhost:9200`],
+    maxRetries: 30,
+    requestTimeout: 30000
+  });
+};
 
-const search = async (itemName) => {
-    let client = await createClient()
+const search = async (itemName, header) => {
+  let client = await createClient();
 
-    let body = {
-        size: 200,
-        from: 0,
-        query: {
-            query_string: {
-                default_field: 'name',
-                query: `*${itemName}*`
-            }
-        }
+  let body = {
+    size: 200,
+    from: 0,
+    query: {
+      query_string: {
+        default_field: "name",
+        query: `*${itemName}*`
+      }
     }
+  };
 
-    return client.search({
-        index: 'market-items',
-        body: body
-    }, {
-        ignore: [404],
-        maxRetries: 1
-    }, (err, {
-        body
-    }) => {
-        if (err) console.log(err)
-    })
-}
-
-const searchId = async (itemId) => {
-    let client = await createClient()
-    let body = {
-        query: {
-            match: {
-                index: itemId
-            }
-        }
+  return client.search(
+    {
+      index: "market-items",
+      body: body
+    },
+    {
+      ignore: [404],
+      maxRetries: 1
+    },
+    (err, { body }) => {
+      if (err) console.log(err);
     }
+  );
+};
 
-    return client.search({
-        index: 'market-items',
-        body: body
-    }, {
-        ignore: [404],
-        maxRetries: 3
-    }, (err, {
-        body
-    }) => {
-        if (err) console.log(err)
-    })
-}
+const searchId = async (itemId, header) => {
+  let client = await createClient();
+  let body = {
+    query: {
+      match: {
+        index: itemId
+      }
+    }
+  };
 
-const createBulk = async () => {
-    let client = await createClient()
-    let bulk = []
+  return client.search(
+    {
+      index: "market-items",
+      body: body
+    },
+    {
+      ignore: [404],
+      maxRetries: 3
+    },
+    (err, { body }) => {
+      if (err) console.log(err);
+    }
+  );
+};
 
-    client.indices.create({
-        index: 'market-items'
-    }, (err, response, status) => {
-        if (err) {
-            return new Error('Creating ES Indices failed');
-        }
-    })
+const createBulk = async header => {
+  let client = await createClient();
+  let bulk = [];
 
-    await items.forEach(item => {
-        item.reviews = undefined
+  client.indices.create(
+    {
+      index: "market-items"
+    },
+    (err, response, status) => {
+      if (err) {
+        return new Error("Creating ES Indices failed");
+      }
+    }
+  );
 
-        bulk.push({
-            index: {
-                _index: 'market-items',
-                _type: 'clothing_list'
-            }
-        })
-        bulk.push(item)
-    })
-    
-    return bulk
-}
+  await items.forEach(item => {
+    item.reviews = undefined;
 
-const importData = async () => {
-    let client = await createClient()
-    const bulk = await createBulk()
-    
-    client.bulk({
-        body: bulk
-    }, (err, response) => {
-        if (err) {
-            return new Error('Failed Bulk operation');
-        }
-    })
-    return bulk
-}
+    bulk.push({
+      index: {
+        _index: "market-items",
+        _type: "clothing_list"
+      }
+    });
+    bulk.push(item);
+  });
+
+  return bulk;
+};
+
+const importData = async header => {
+  let client = await createClient();
+  const bulk = await createBulk();
+
+  client.bulk(
+    {
+      body: bulk
+    },
+    (err, response) => {
+      if (err) {
+        return new Error("Failed Bulk operation");
+      }
+    }
+  );
+  return bulk;
+};
 
 module.exports = Object.assign({
-    search,
-    searchId,
-    importData
-})
+  search,
+  searchId,
+  importData
+});
--- api/index.js
+++ api/index.js
@@ -1,71 +1,80 @@
-const redis = require('./app/redis')
-const elastic = require('./app/elastic')
+const redis = require("./app/redis");
+const elastic = require("./app/elastic");
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-let specialOffers = process.env.ES_SPECIAL_OFFER || true
-let totalOffers = process.env.ES_TOTAL_OFFER || 1
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let specialOffers = process.env.ES_SPECIAL_OFFER || true;
+let totalOffers = process.env.ES_TOTAL_OFFER || 1;
 
-app.use(bodyParser.json())
-app.set('port', process.env.PORT || 3001)
+app.use(bodyParser.json());
+app.set("port", process.env.PORT || 3001);
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.send('(v1) Hello World! Marketplace with sales and reviews made with <3 by the OCTO team at Kong Inc.')
-})
+app.get("/", (req, res) => {
+  res.set(req.headers);
+  res.send(
+    "(v1) Hello World! Marketplace with sales and reviews made with <3 by the OCTO team at Kong Inc."
+  );
+});
 
-app.post('/upload', async (req, res) => {
-  await redis.importData()
-  await elastic.importData()
-  res.end('Mock data updated in Redis and ES!')
-})
+app.post("/upload", async (req, res) => {
+  await redis.importData();
+  await elastic.importData();
+  res.end("Mock data updated in Redis and ES!");
+});
 
-app.get('/items', (req, res) => {
-  elastic.search(req.query.q)
+app.get("/items", (req, res) => {
+  elastic
+    .search(req.query.q, req.headers)
     .then(async results => {
+      res.set(req.headers);
       if (specialOffers == true) {
-        res.send(addOffer(results.hits.hits))
+        res.send(addOffer(results.hits.hits));
       } else {
-        res.send(results.hits.hits)
+        res.send(results.hits.hits);
       }
     })
     .catch(err => {
-      res.send(err)
-    })
-})
+      res.send(err);
+    });
+});
 
-let addOffer = (arr) => {
-  let items = arr
+let addOffer = arr => {
+  let items = arr;
   for (i = 0; i < totalOffers; i++) {
-    items[i]._source.specialOffer = true
+    items[i]._source.specialOffer = true;
   }
-  return items
-}
+  return items;
+};
 
-app.get('/items/:itemIndexId', (req, res) => {
-  elastic.searchId(req.params.itemIndexId)
+app.get("/items/:itemIndexId", (req, res) => {
+  elastic
+    .searchId(req.params.itemIndexId, req.headers)
     .then(results => {
-      res.send(results.hits.hits)
+      res.set(req.headers);
+      res.send(results.hits.hits);
     })
     .catch(err => {
-      res.send(err)
-    }) 
-})
+      res.send(err);
+    });
+});
 
-app.get('/items/:itemIndexId/reviews', (req, res) => {
-  redis.search(`${req.params.itemIndexId}`)
+app.get("/items/:itemIndexId/reviews", (req, res) => {
+  redis
+    .search(`${req.params.itemIndexId}`, req.headers)
     .then(results => {
-      res.send(results)
+      res.set(req.headers);
+      res.send(results);
     })
     .catch(err => {
-      res.send(err)
-    })
-})
+      res.send(err);
+    });
+});
 
-app.listen(app.get('port'))
+app.listen(app.get("port"));
