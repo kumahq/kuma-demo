18d0220e985ad4362e7eb5ecdba21280c662fe2a
James Peach <james.peach@konghq.com>
2021-09-06 18:33:11 +1000

    fix(vagrant) default to installing latest Kuma (#125)
    This fixes #121.
    
    Signed-off-by: James Peach <james.peach@konghq.com>

--- vagrant/common/download_kuma.sh
+++ vagrant/common/download_kuma.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ -z "${KUMA_VERSION}" ]; then
-  echo "Error: environment variable KUMA_VERSION is not set"
-  exit 1
-fi
-
 if [ -z "${KUMA_HOME}" ]; then
   echo "Error: environment variable KUMA_HOME is not set"
   exit 1
@@ -15,13 +10,23 @@ fi
 # Configure Kuma in all interactive bash shells if user SSH into machine
 echo "
 export KUMA_HOME=${KUMA_HOME}
-export KUMA_VERSION=kuma-${KUMA_VERSION}
 export PATH=\$PATH:\$KUMA_HOME/bin
 " > /etc/profile.d/kuma.sh
 
-# Download specified version of Kuma for the detected OS, please check out https://kuma.io/install for more options
-curl --silent https://kuma.io/installer.sh | VERSION=$KUMA_VERSION sh >/dev/null 2>&1
+ARCHIVE_DIR=`mktemp -d`
+cd "$ARCHIVE_DIR"
 
-# Move to KUMA_HOME directory 
-mv kuma-${KUMA_VERSION} ${KUMA_HOME}
-export PATH=$PATH:$KUMA_HOME/bin
+# Download Kuma for the detected OS, please check out https://kuma.io/install
+# for more options. If $KUMA_VERSION is set, dowload that version, otherwise
+# download the latest.
+if [ -n "$KUMA_VERSION" -a "$KUMA_VERSION" != "latest" ]; then
+    curl --silent https://kuma.io/installer.sh | VERSION="$KUMA_VERSION" sh >/dev/null 2>&1
+else
+    curl --silent https://kuma.io/installer.sh | sh >/dev/null 2>&1
+fi
+
+# Copy Kuma binaries from the versioned download to the VM home directory.
+mkdir -p "$KUMA_HOME/bin/"
+cp -r kuma-*/bin/* "$KUMA_HOME/bin/"
+
+rm -rf kuma-*
