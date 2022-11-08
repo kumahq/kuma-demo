d95f867a44be348596a17023bea22fa95c43fffe
Bart Smykla <bartek@smykla.com>
2022-11-08 10:51:22 +0100

    fix: fix vagrant setup
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- vagrant/postgresql/app/start.sh
+++ vagrant/postgresql/app/start.sh
@@ -3,13 +3,13 @@ echo '===== Creating PostgreSQL databases and users ====='
 su postgres
 
 # Update pg_hba.conf to trust local peer connection
-sed -i  '/^local   all             all                                     peer/ s/peer/trust/' /etc/postgresql/13/main/pg_hba.conf
-cat /etc/postgresql/13/main/pg_hba.conf
+sed -i  '/^local   all             all                                     peer/ s/peer/trust/' /etc/postgresql/15/main/pg_hba.conf
+cat /etc/postgresql/15/main/pg_hba.conf
 
 # Restart Postgres to udpate conf
 /etc/init.d/postgresql restart
 
-# Create kumademo user 
+# Create kumademo user
 sudo -u postgres psql -c "CREATE USER kumademo WITH SUPERUSER CREATEDB ENCRYPTED PASSWORD 'kumademo'"
 
 # Create kumademo database
